cd52c338faf50460eface6d3de549990f521afd0
DeepLearning <50433241+huangpan2507@users.noreply.github.com>
2020-03-24 17:56:51 +0800

    Update QLineEditValidator.py

--- src/controls/QLineEditValidator.py
+++ src/controls/QLineEditValidator.py
@@ -47,7 +47,7 @@ class QLineEditValidator(QWidget):
         doubleValidator.setDecimals(2)
 
         # 字符和数字
-        reg = QRegExp('[a-zA-z0-9]+$')                                          # 大小写以及数字
+        reg = QRegExp('[a-zA-Z0-9]+$')                                          # 大小写以及数字
         validator = QRegExpValidator(self)
         validator.setRegExp(reg)
 
