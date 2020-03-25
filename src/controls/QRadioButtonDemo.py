40ce3ab24a296551d79b06d56b86005a9d55f395
DeepLearning <50433241+huangpan2507@users.noreply.github.com>
2020-03-25 11:36:52 +0800

    Update QRadioButtonDemo.py
    self.btn2.toggled.connect(lambda: self.btnstate(self.btn2))： 当选择两个按钮相互切换时，按钮的状态发生改变，将触发toggle信号，并与槽函数btnstate（）连接。使用lamdba的方式允许将源信号传递给槽函数，将按钮作为参数

--- src/controls/QRadioButtonDemo.py
+++ src/controls/QRadioButtonDemo.py
@@ -24,6 +24,7 @@ class QRadioButtonDemo(QWidget):
 
         self.button2 = QRadioButton('单选按钮2')
         self.button2.toggled.connect(self.buttonState)
+        
         layout.addWidget(self.button2)
         self.setLayout(layout)
 
@@ -35,7 +36,18 @@ class QRadioButtonDemo(QWidget):
             print('<' + radioButton.text() + '> 被选中')
         else:
             print('<' + radioButton.text() + '> 被取消选中状态')
-
+    """
+    def btnstate(self,btn):
+        if btn.isChecked() == True:
+               print('<' + btn.text() + '> 被选中')
+        else:
+            print('<' + btn.text() + '> 被取消选中状态')
+            
+      # 当选择两个按钮相互切换时，按钮的状态发生改变，将触发toggle信号，并与槽函数btnstate（）连接。使用lamdba的方式允许将源信号传递给槽函数，将按钮作为参数     
+      则 可以改为 self.btn1.toggled.connect(lambda :self.btnstate(self.btn1))
+      self.btn2.toggled.connect(lambda: self.btnstate(self.btn2))            
+      
+    """
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
