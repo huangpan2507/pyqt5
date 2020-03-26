c44fb9d02e80953c7ff0690a9845e5be19d031a5
DeepLearning <50433241+huangpan2507@users.noreply.github.com>
2020-03-27 00:49:57 +0800

    Update DrapDrop.py

--- src/drapclip/DrapDrop.py
+++ src/drapclip/DrapDrop.py
@@ -17,19 +17,19 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
-class MyComboBox(QComboBox) :
+class MyComboBox(QComboBox) :　　　　          #　我们需要重新实现某些方法才能使QComboBox接受拖放操作。因此我们创建了继承自QComboBox的MyComboBox类。
     def __init__(self):
         super(MyComboBox,self).__init__()
         self.setAcceptDrops(True)
-    def dragEnterEvent(self,e):
+    def dragEnterEvent(self,e):               # 拖动开始时，以及刚进入目标控件时调用
         print(e)
         #检测拖曳进来的数据是否包含文本，如有则接受，无则忽略
         if e.mimeData().hasText():
             e.accept()
         else:
             e.ignore()
-    def dropEvent(self,e):
-        self.addItem(e.mimeData().text())
+    def dropEvent(self,e):                     # 重新实现了dragEnterEvent()方法
+        self.addItem(e.mimeData().text())      # 我们定义了在drop事件发生时的行为。这里我们改变了按钮的文字。
 
 class DrapDropDemo(QWidget):
     def __init__(self):
@@ -37,7 +37,7 @@ class DrapDropDemo(QWidget):
         formLayout = QFormLayout()
         formLayout.addRow(QLabel("请将左边的文本拖拽到右边的下拉列表中"))
         lineEdit = QLineEdit()
-        lineEdit.setDragEnabled(True)  # 让QLineEdit控件可拖动
+        lineEdit.setDragEnabled(True)  # 让QLineEdit控件可拖动 ,QLineEdit内置了对drag(拖动)操作的支持。我们只需要调用setDragEnabled()方法就可以了。
 
         combo = MyComboBox()
         formLayout.addRow(lineEdit,combo)
